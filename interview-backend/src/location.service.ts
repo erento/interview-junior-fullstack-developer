cb877bf024e159568cc3ad3d5421e28a99c315aa
janlesjak <lesjakov.jan@gmail.com>
2023-08-09 16:44:25 +0200

    Type corrections

--- interview-backend/src/location.service.ts
+++ interview-backend/src/location.service.ts
@@ -5,6 +5,7 @@ export interface City {
   cityName: string
   count: number
 }
+
 export interface LocationPagination{
   page: number
   numberOfPages: number
@@ -15,22 +16,22 @@ const cityList: City[] = require('../cities.json');
 @Injectable()
 export class LocationService {
   getLocation(cityName: string, page: number, pageSize: number = 5): LocationPagination {
-    const arr = this.getCityName();
-    const filteredCities = arr.filter(
-      (el) => el.toLowerCase().includes(cityName.toLowerCase())
+    const arr: string[] = this.getCityName();
+    const filteredCities: string[] = arr.filter(
+      (el: string) => el.toLowerCase().includes(cityName.toLowerCase())
       );
-    const numberOfCities = filteredCities.length;
-    const startIndex = (page - 1) * pageSize;
-    const endIndex = startIndex + pageSize;
+    const numberOfCities: number = filteredCities.length;
+    const startIndex: number = (page - 1) * pageSize;
+    const endIndex: number = startIndex + pageSize;
     return {
       results: filteredCities.slice(startIndex, endIndex), 
-      numberOfPages: Math.ceil(numberOfCities/pageSize),
+      numberOfPages: Math.ceil(numberOfCities / pageSize),
       page
     }
   }
   
   getCityName(): string[] {
-    const cityNameFound = cityList.map(cityName => cityName.cityName);
+    const cityNameFound: string[] = cityList.map(cityName => cityName.cityName);
     return cityNameFound;
   }
 }
